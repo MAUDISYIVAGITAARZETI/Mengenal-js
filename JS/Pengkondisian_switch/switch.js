21bf943110813ab05fb2c7d42283df43d0fd5671
MAUDISYIVAGITAARZETI <99390231+MAUDISYIVAGITAARZETI@users.noreply.github.com>
2022-03-09 14:30:55 +0700

    js

---
+++ JS/Pengkondisian_switch/switch.js
@@ -0,0 +1,22 @@
+var item = prompt('masukkan nama makanan / minuman : \n (cth: nasi, daging, susu, hamburger, softdrink)');
+
+switch( item ) {
+	case 'nasi' :
+		alert('makanan / minuman SEHAT!');
+		break;
+	case 'daging' :
+		alert('makanan / minuman SEHAT!');
+		break;
+	case 'susu' :
+		alert('makanan / minuman SEHAT!');
+		break;
+	case 'hamburger' :
+		alert('makanan / minuman TIDAK SEHAT!');
+		break;
+	case 'softdrink' :
+		alert('makanan / minuman TIDAK SEHAT!');
+		break;
+	default :
+		alert('anda memasukkan makanan / minuman yang salah!');
+		break;
+}
